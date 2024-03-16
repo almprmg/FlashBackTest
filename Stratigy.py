33953e2505e0961cb2dbf89ccdffd04c2ff23f11
almprmg <hhyyuu0990@gmail.com>
2024-03-17 02:24:46 +0300

    skip bast

--- Stratigy.py
+++ Stratigy.py
@@ -1,7 +1,7 @@
 
 
 import pandas as pd
-from BackTest_Smull import Bacl_Test_smunll
+from BackTest.BackTest_Smull import Bacl_Test_smunll
 
 
 class Stratigy(Bacl_Test_smunll):
@@ -9,11 +9,11 @@ class Stratigy(Bacl_Test_smunll):
     sl =None
     limet =None
     def __init__(self,data,data_low) -> None:
-        self.data_low = data_low.loc[data_low.Date > pd.Timestamp('2022-09-02 17:00:00')]
+        self.data_low = self.data_low.loc[data_low.Date >= pd.Timestamp(data.Date[-1])]
         self.data=data 
         self._postin = False
         self.Date_ende_order = None
-  
+        super()
         
         
     def buy(self, limet,tp: float,sl:float):
@@ -21,13 +21,6 @@ class Stratigy(Bacl_Test_smunll):
             self.tp: float =tp 
             self.sl: float =sl
             self.limet: float =limet
-            data = self.data_low.loc[self.data_low.Date < pd.Timestamp('2022-09-02 18:00:00')]
-            low_limt =  data_low.loc[data.High >= limet,'Low']
-            low_tp =  data_low.loc[data_low.Low >= self.tp,'Low']
-            self.check = low_limt.index[0] < low_tp.index[0]
-           
-
-
             
             
     def Check_Order(self):
