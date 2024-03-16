e78e3b6bc1ee3d3e64ed56d1916367a594c21e94
almprmg <hhyyuu0990@gmail.com>
2024-03-17 02:21:13 +0300

    fultter buy rell

--- BackTest_Smull.py
+++ BackTest_Smull.py
@@ -1,13 +1,16 @@
 
 
+import pandas as pd
+
+
 class Bacl_Test_smunll:
-    def __init__(self,Start_Date,data_low) -> None:
-        self.data_low = data_low.loc[Start_Date]
+    def __init__(self) -> None:
+        self.data_low = None
         self.tp = None
-        self.sl - None
+        self.sl = None
     def next(self):
         high =  self.data_low.loc[self.data_low.High >= self.tp,'High']
-        Low =  self.data_low.loc[self.data_low.Low >= self.sl,'Low']
+        Low =  self.data_low.loc[self.data_low.Low <= self.sl,'Low']
         check = high.index[0] < Low.index[0]
         if check :
             return self.data_low.Date[high.index[0]] 
