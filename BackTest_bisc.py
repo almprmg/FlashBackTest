9689c5f62a490c8da3dee53c5bd46f55d9027971
almprmg <hhyyuu0990@gmail.com>
2024-03-18 00:43:00 +0300

    added endDate and fixed code

--- BackTest_bisc.py
+++ BackTest_bisc.py
@@ -5,11 +5,12 @@ from Stratigy import Stratigy
 
 
 class BackTest:
-    def __init__(self,data : pd.DataFrame,data_smull,Strutigy :type[Stratigy] ) -> None:
-        self.data1 = data
-        self.data = None
+    def __init__(self,data : pd.DataFrame,data_smull,Stratigy :type[Stratigy] ) -> None:
+        self.data = data
         self.data_smull = data_smull  
-        self.Strutigy = Strutigy
+        self.Strutigy = Stratigy
+        self.endDate =self.data.index[-1]
+
         if not isinstance(data,pd.DataFrame):
             raise
         if not isinstance(data.index,pd.Timestamp()):
@@ -17,10 +18,18 @@ class BackTest:
          
 
     def run(self):
-        self.Strutigy.inint
-        for index in range(0,len(self.data1)-1):
-            self.data = self.data1[:index,:]
-            self.Strutigy.next()
+        Stratigy = self.Strutigy(self.data_smull)
+        Stratigy.init()
+        if "Signal" in self.data.columns:
+            Stratigy.Date_ende_order = Stratigy.Date =  self.data.loc[self.data.Signal != 0 ].index[0]
+
+            while self.endDate < Stratigy.Date_ende_order :
+                
+                Stratigy.Data = self.data.loc[ self.data.index >= Stratigy.Date ]
+                Stratigy.next()
+                
+                
+                
             
 
     
