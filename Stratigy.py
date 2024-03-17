bf89dd2ddea457402c40bd0e8ab64440d210d110
almprmg <hhyyuu0990@gmail.com>
2024-03-17 22:59:18 +0300

    Add sell function and pram... storg  order & orders

--- Stratigy.py
+++ Stratigy.py
@@ -5,24 +5,26 @@ from BackTest.BackTest_Smull import Bacl_Test_smunll
 
 
 class Stratigy(Bacl_Test_smunll):
-    tp = None
-    sl =None
-    limet =None
-    def __init__(self,data,data_low) -> None:
-        self.data_low = self.data_low.loc[data_low.Date >= pd.Timestamp(data.Date[-1])]
-        self.data=data 
+    order = None
+    
+    order_ ={"DateStart" : [],"Enter" : [] ,"Tp" : [],"Sl" : [] ,"Targit" : [],"DateEnd" : []}
+    def __init__(self,data_low,Date) -> None:
+        self.data_low = self.data_low.loc[data_low.index >= pd.Timestamp(Date)]
+        self.Date = Date
         self._postin = False
         self.Date_ende_order = None
+        
         super()
         
         
     def buy(self, limet,tp: float,sl:float):
         if not self._postin:
-            self.tp: float =tp 
-            self.sl: float =sl
-            self.limet: float =limet
+            self.order = [self.Date,limet,tp,sl]
             
-            
+    def sell(self, limet:float,tp: float,sl:float):
+        if not self._postin:
+            self.order = [self.Date,limet,tp,sl]
+               
     def Check_Order(self):
         self.Date_ende_order = super().next()
 
