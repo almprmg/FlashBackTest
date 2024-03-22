437918763abbcf9afd690012c41edfb5f84029be
almprmg <hhyyuu0990@gmail.com>
2024-03-22 20:23:52 +0300

    add function  fees,initCash profit and rplace name function quntum to quantitySymbol

--- Calculator.py
+++ Calculator.py
@@ -2,12 +2,46 @@ import pandas as pd
 
 
 class Calculator_Traded:
-    def different (result: type[pd.DataFrame]):
-        result['Order_end']  =  [row["tp"] if row['Target'] == 1 else abs(row["sl"])  for row in result]
-        result['different'] = result[['Enter','Order_end']].diff(axis=1)
+    """
+    First discrete difference of element.
+        .......et
+    Parameters
+    ----------
+    Cash 
+    RatioEntry
+    CP = cumulative profit
+    """
+ 
+    def __init__(self,Cash : int = 1000 ,RatioEntry :int =100 ,Fees : int = None, CP : bool = False ) -> None:
+        
+        self.Cash =  Cash
+        self.amount : float
+        self.RatioEntry = RatioEntry
+        self.CP = CP
+        self.Retail = int
+        self.Fees =Fees
+        
+    def initCash(self):
+        self.Retail = 100 / self.RatioEntry 
+        self.amount = self.Cash /  self.Retail
+
+    def fees(self):
+        Fees = self.amount * self.Fees
+        self.amount = self.amount -Fees
+    def different(self,result: type[pd.DataFrame]):
+        result['PriceEndOrder']  =  [row["tp"] if row['Target'] == 1 else row["sl"]  for _,row in result.iterrows()]
+        result['different'] = result[['Enter','PriceEndOrder']].diff(axis=1)['PriceEndOrder']
         return result
-    def Quantum_(result: type[pd.DataFrame]):
-        Enter = result['Enter']
+    
+    
+    def quantitySymbol(self,result: type[pd.DataFrame]):
+        result['Quantity'] = result['Enter'] * self.amount
+        return result
+
+    def profit(self,result: type[pd.DataFrame]):
+        result['Profit'] = result['Quantity'] * result['different']
+        return result
+
 
 
 
