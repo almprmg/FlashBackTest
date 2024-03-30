cf96c69a892e112f4fb4d57babe043fa8117759a
almprmg <hhyyuu0990@gmail.com>
2024-03-31 01:22:33 +0300

    convert  order data_frame to dataclass ,And creating the functions that the class needs

--- order.py
+++ order.py
@@ -1,9 +1,34 @@
+from dataclasses import dataclass
 import pandas as pd
 
 class _unit:
     @staticmethod
     def swap(first,secund):
         return  secund ,first
+@dataclass
+class DataOrder:
+    id: int = None
+    type_: int =None
+    date_starting: pd.Timestamp = None
+    #symbol: str
+    #quantity: float = None
+    price: float = None
+    tp: float = None
+    sl: float = None
+    success: int = None
+    date_end: pd.Timestamp = None
+    def getOrder(self) -> list:
+        ""
+        return  [self.id ,
+                self.type_ ,
+                self.date_starting ,
+                self.price ,
+                self.tp ,
+                self.sl ,
+                self.success ,
+                self.date_end ,
+                ]
+
 
 class Orders:
     def __init__(self,date_start_order,data,data_low) -> None:
@@ -16,15 +41,8 @@ class Orders:
                                             "Target":[],
                                             "EndDate":[]
                                             })
-        self.__order = pd.DataFrame({"IdOrder":[0],
-                                     "Type":[0],
-                                     "DateStart":[0] ,
-                                     "Enter":[0] ,
-                                     "tp":[0],
-                                     "sl":[0] ,
-                                     "Target":[0],
-                                     "EndDate":[0]
-                                     })
+
+        self.__order : DataOrder
         self._type = None
         self._date_tp = None
         self._date_sl = None
@@ -40,9 +58,9 @@ class Orders:
 
 
     def _process_order(self):
-            if self.is_long():
-                return self.long_order()
-            self.short_order()
+        if self.is_long():
+            return self.long_order()
+        self.short_order()
 
     def long_order(self):
         self.date_finish_order()
@@ -69,16 +87,15 @@ class Orders:
     def _open_order(self ,type_order ,limit ,tp ,sl ):
         self._position = True
         self._type =type_order
-        self.__order[["IdOrder" ,
-                      "Type",
-                      "DateStart" ,
-                      "Enter" ,
-                      "tp" ,
-                      "sl"]] = [str(type_order)+str(len( self.result_orders)),
-                                type_order,self.date_start_order,limit,tp,sl]
+        self.__order =  DataOrder(str(type_order)+str(len( self.result_orders)),
+                     type_order,
+                     self.date_start_order,limit
+                     ,tp,
+                     sl,)
 
-    def _close_order(self,goal:bool ,date : pd.Timestamp) -> None :
-        self.__order[[ "Target","EndDate"]] = [goal,date]
-        self.date_end_order = date
+    def _close_order(self,goal:bool ,date_end : pd.Timestamp) -> None :
+        self.__order.success,self.__order.date_end = goal , date_end
+        self.date_end_order = date_end
         self._position =False
+        print(self.__order)
         self.result_orders = pd.concat([self.result_orders,self.__order ] ,axis=0,ignore_index=True)
