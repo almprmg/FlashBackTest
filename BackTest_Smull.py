85fd4b309da070be746e63cdf987a85593f648b0
almprmg <hhyyuu0990@gmail.com>
2024-04-06 02:20:37 +0300

    delet all  condtion if equl null and add condtion is_position

--- BackTest_Smull.py
+++ BackTest_Smull.py
@@ -6,31 +6,25 @@ class BestTestLow(Orders):
 
     def trade(self):
         self._process_order()
-        if self._date_tp or self._date_sl :
-            if self.is_unknown() :
-                if self.is_win():
-                    return
-                self.is_loss()
+
+        if self.is_position() and self.is_unknown() :
+            if self.is_win():
+                return
+            self.is_loss()
     def is_unknown(self):
-        if not self._date_tp  and self._date_sl :
-            self._close_order(0,self._date_sl)
-            return False
-        elif self._date_tp == self._date_sl:
-            self._close_order(0,self._date_sl)
-            return False
+        is_unknown = self._date_tp == self._date_sl
+        if is_unknown:
+            return  not  self._close_order(0,self._date_sl)
         return True
 
     def is_win(self):
-        if  self._date_tp and not self._date_sl :
-            self._close_order(1 ,self._date_tp)
-            return True
-        elif self._date_tp < self._date_sl :
-            self._close_order(1 ,self._date_tp)
-            return True
+        is_done_tp = self._date_tp < self._date_sl
+        if is_done_tp:
+            return not self._close_order(1 ,self._date_tp)
         return False
 
     def is_loss(self):
-        if  self._date_tp > self._date_sl:
-            self._close_order(0 ,self._date_sl)
-            return True
+        is_done_sl = self._date_tp > self._date_sl
+        if  is_done_sl :
+            return not self._close_order(0,self._date_sl)
         return False
