6269c82df8b7b4ac49aefa5e002aaac486f0ad4c
almprmg <hhyyuu0990@gmail.com>
2024-04-14 22:52:33 +0300

    fixed error CLASS  UnCumulativeTradeProfit

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta ,abstractmethod
+from time import sleep
 import pandas as pd
 from dataclasses import dataclass,field
 from _util import adding_threezero
@@ -21,21 +22,7 @@ class Wallet:
 
         return int(abs(row * self.rate_fees))
 class InitDataTraded():
-    @staticmethod
-    def different(func) -> pd.DataFrame :
-        """_summary_
 
-        Args:
-            func (_type_): _description_z
-
-        Returns:
-            pd.DataFrame: _description_
-        """
-        def fuc_wrapper(self,result):
-            result['different'] =  result['PriceEndOrder'] - result['price'] if result['type_order'] == 1 else  result['price'] - result['PriceEndOrder']
-            result = func(self,result)
-            return result
-        return fuc_wrapper
 
     @staticmethod
     def quantity_symbol(func) -> pd.DataFrame:
@@ -46,7 +33,6 @@ class InitDataTraded():
             return  func(self,result)
         return fuc_wrapper
     @staticmethod
-
     def profit_trade(func)-> pd.DataFrame:
 
         def fuc_wrapper(self,result):
@@ -56,8 +42,13 @@ class InitDataTraded():
             return func(self,result)
         return fuc_wrapper
     @staticmethod
-    def price_end(result: pd.DataFrame =None ) -> list:
-        return [row["tp"] if row['success'] != 0 else row["sl"] for _,row in result.iterrows()]
+    def price_end(order: pd.DataFrame  ) -> list:
+
+        return [row["tp"] if row["success"] != 0 else row["sl"] for _,row in order.iterrows()]
+    
+    @staticmethod
+    def different(order: pd.DataFrame) -> list:
+        return [row['PriceEndOrder'] - row['price'] if row['type_order'] == 1 else  row['price'] - row['PriceEndOrder'] for _,row in order.iterrows()]
 
 class CalcutatorPofit(metaclass=ABCMeta):
     def __init__(self, cash, ratio_entry, fees) -> None:
@@ -83,7 +74,8 @@ class CalcutatorPofit(metaclass=ABCMeta):
 class UnCumulativeTradeProfit(CalcutatorPofit):
 
 
-    @InitDataTraded.different
+
+
     @InitDataTraded.quantity_symbol
     @InitDataTraded.profit_trade
     def howto_win(self,result:pd.DataFrame) -> pd.DataFrame:
@@ -92,10 +84,13 @@ class UnCumulativeTradeProfit(CalcutatorPofit):
         ep_result = result[["price","PriceEndOrder"]]
         result['PctProfit'] = ep_result.pct_change(axis="columns",periods=1)["PriceEndOrder"] * 100
         return result
+
     def profit(self,result: pd.DataFrame) -> pd.DataFrame:
         """_summary_
         """
-        result['PriceEndOrder'] = InitDataTraded.price_end(result)
+  
+        result["PriceEndOrder"] = InitDataTraded.price_end(result)
+        result['different'] =   InitDataTraded.different(result)
         return self.fixed(self.howto_win(result))
 
 class CumulativeTradeProfit(CalcutatorPofit):
@@ -103,14 +98,13 @@ class CumulativeTradeProfit(CalcutatorPofit):
         signature as the implementation in CalculatorProfit.
     """
     InitData =InitDataTraded
-    @InitData.different
     @InitData.quantity_symbol
     @InitData.profit_trade
     def howto_win(self ,result:pd.DataFrame) -> pd.DataFrame:
         self.wallet.cash += result['ProfitTrade']
         result['CumProfit'] = self.wallet.cash
         ep_result = result[["price" ,"PriceEndOrder"]]
-        result['PctProfit'] = ep_result.pct_change(periods=1)["PriceEndOrder"]*100
+        result['PctProfit'] = ep_result.pct_change(periods=1)["PriceEndOrder"] * 100
         return  result
 
     def profit(self ,result : pd.DataFrame)-> pd.DataFrame:
@@ -123,4 +117,5 @@ class CumulativeTradeProfit(CalcutatorPofit):
             pd.DataFrame: _description_
         """
         result['PriceEndOrder'] = InitDataTraded.price_end(result)
+        result['different'] =   InitDataTraded.different(result)
         return  self.fixed(result.apply(self.howto_win ,axis = 1 ))
