daf8464a6a7ef7cdf9937e459afea223b6424c66
almprmg <hhyyuu0990@gmail.com>
2024-04-18 00:27:43 +0300

    update _util.py

--- _util.py
+++ _util.py
@@ -1,4 +1,7 @@
+from numbers import Number
+from typing import Union
 from pandas import Series
+from pandas import Timedelta
 
 def is_empty(date_target :Series , date_loss :Series ) -> bool:
 
@@ -11,3 +14,8 @@ def adding_threezero(num1):
 
 
     return int(f"{num1}{0}{0}{0}")
+
+def _data_period(index) -> Union[Timedelta, Number]:
+    """Return data index period as pd.Timedelta"""
+    values = Series(index[-100:])
+    return values.diff().dropna().median()
