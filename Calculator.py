ea1f7f22c21ef33212d9436f4af90d6cc40fbeee
almprmg <hhyyuu0990@gmail.com>
2024-04-21 22:14:11 +0300

    update rename same argmeint Calculator.py.py

--- Calculator.py
+++ Calculator.py
@@ -19,15 +19,13 @@ class Wallet:
         retail = 100 / self.ratio_entry
         return self.cash /  retail
 
-
-
-
-class CalcutatorProfit():
+class CalculatorProfit:
     """Calculator profit trade of ech order."""
-    def __init__(self, cash, ratio_entry, rate_fees,cp) -> None:
+    def __init__(self, cash, ratio_entry, rate_fees,cp,data_ohlc) -> None:
         self.wallet = Wallet( cash, ratio_entry)
         self.rate_fees : float = rate_fees
         self.__cp = cp
+        self.__data_ohlc = data_ohlc
     def fees(self,amount:int | pd.Series) -> int | pd.Series:
         """fess trade open,close."""
         return self.rate_fees * amount
@@ -40,42 +38,43 @@ class CalcutatorProfit():
         """Return PnL cash."""
         return self.wallet.amount * pct_cheng
 
-    def fixed_result(self,result:pd.DataFrame):
-        result["CumProfit"] -= (result["FeesClose"] + result["FeesOpen"])
-        result.drop(columns=["FeesClose","FeesOpen"],inplace=True)
-        result["CumProfit"] /=1000
-        result["PnL"] /=1000
-        result["Quantity"] /=1000
-        return result
-    def unCumulative(self,result:pd.DataFrame) -> pd.DataFrame:
+    def fixed_result(self,orders:pd.DataFrame):
+        orders["CumProfit"] -= (orders["FeesClose"] + orders["FeesOpen"])
+        orders.drop(columns=["FeesClose","FeesOpen"],inplace=True)
+        orders["CumProfit"] /=1000
+        orders["PnL"] /=1000
+        orders["Quantity"] /=1000
+        return orders
+    def uncumulative(self,orders:pd.DataFrame) -> pd.DataFrame:
         """There is no need to override 'price_end' it has the same
             signature as the implementation in UnCalculatorProfit.
         """
-        result["FeesOpen"] = self.fees(self.wallet.amount)
-        result["Quantity"] = self.quantity( result.enter_price)
-        result['PnL'] = self.pnl(result['pct'])
-        result['FeesClose'] = self.fees(result['PnL']+self.wallet.amount)
-        result['CumProfit'] = result['PnL'].cumsum() + self.wallet.cash
-        return result
+        orders["FeesOpen"] = self.fees(self.wallet.amount)
+        orders["Quantity"] = self.quantity( orders.enter_price)
+        orders['PnL'] = self.pnl(orders['pct'])
+        orders['FeesClose'] = self.fees(orders['PnL']+self.wallet.amount)
+        orders['CumProfit'] = orders['PnL'].cumsum() + self.wallet.cash
+        return orders
 
-    def cumulative(self ,result:pd.DataFrame) -> pd.DataFrame:
+    def cumulative(self ,order:pd.DataFrame) -> pd.DataFrame:
         """There is no need to override 'price_end' it has the same
             signature as the implementation in CalculatorProfit.
         """
-        result["FeesOpen"] = self.fees(self.wallet.amount)
-        result["Quantity"] = self.quantity( result.enter_price)
-        result['PnL'] = self.pnl(result['pct'])
-        result['FeesClose'] = self.fees(result['PnL']+self.wallet.amount)
-        self.wallet.cash += result['PnL']
-        result['CumProfit'] = self.wallet.cash
-        return result
+        order["FeesOpen"] = self.fees(self.wallet.amount)
+        order["Quantity"] = self.quantity( order.enter_price)
+        order['PnL'] = self.pnl(order['pct'])
+        order['FeesClose'] = self.fees(order['PnL']+self.wallet.amount)
+        self.wallet.cash += order['PnL']
+        order['CumProfit'] = self.wallet.cash
+        return order
 
     def profit(self,trade:pd.DataFrame)-> pd.DataFrame:
-        trade =  trade.apply(self.cumulative ,axis = 1) if  self.__cp else (self.unCumulative(trade))
+        trade =  trade.apply(self.cumulative ,axis = 1) if  self.__cp else (self.uncumulative(trade))
         return self.fixed_result(trade)
-    def finaly_result(self,result_trade,ohlc_data):
+    def result(self,result_trade):
 
-        return compute_stats(self.profit(result_trade),ohlc_data)
+        return compute_stats(self.profit(result_trade),
+                             self.__data_ohlc)
 
 
 
