83478b15b9075cd7f33ceb226d167f5395a1cb88
almprmg <hhyyuu0990@gmail.com>
2024-04-21 22:13:24 +0300

    update _util.py

--- _util.py
+++ _util.py
@@ -4,12 +4,12 @@ from pandas import Series
 from pandas import Timedelta
 
 def is_empty(date_target :Series , date_loss :Series ) -> bool:
-
         return (date_target or date_loss)
 
 def check_empty(series_data : Series):
 
         return False if series_data.empty else series_data.index[-1]
+
 def adding_threezero(num1):
 
 
